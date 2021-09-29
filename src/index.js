efb0d348ffa23ff1227ec9d43f7afb83d6e2a766
niboshi <niboshiporipori@gmail.com>
2021-09-29 20:59:15 +0900

    Initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import "./styles.css";
-
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// <link rel="preconnect" href="https://fonts.googleapis.com">
-// <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-// <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
-// `;
