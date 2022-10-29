e2a8d2427ff50d5ed6e146e23e80027b1260c665
niboshi <niboshiporipori@gmail.com>
2022-10-29 14:01:23 +0900

    Add cypress (#109)
    * Add Cypress
    
    * Setup Cypress on CI
    
    * format
    
    * Exclude cypress directory on tsconfig

---
+++ cypress/e2e/main.cy.ts
@@ -0,0 +1,33 @@
+describe("nibo.sh page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("displays Scrapbox link", () => {
+    cy.get('[data-cy="link-scrapbox"]').should(
+      "have.attr",
+      "href",
+      "https://scrapbox.io/niboshi/niboshi"
+    );
+  });
+
+  it("displays Twitter link", () => {
+    cy.get('[data-cy="link-twitter"]').should(
+      "have.attr",
+      "href",
+      "https://twitter.com/mpppk"
+    );
+  });
+
+  it("displays GitHub link", () => {
+    cy.get('[data-cy="link-github"]').should(
+      "have.attr",
+      "href",
+      "https://github.com/mpppk"
+    );
+  });
+
+  it("displays Pay link", () => {
+    cy.get('[data-cy="link-pay"]').click().get("h1").should("have.text", "Pay");
+  });
+});
